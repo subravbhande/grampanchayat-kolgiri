e44cf5ebd252f37ba7a685e7599c1d8db3c1dd36
Your Name <yourgithubemail@gmail.com>
2026-01-06 00:11:09 +0530

    fix: disable default spring security in boot 3

--- src/main/java/com/smartvillage/smart_village_backend/config/SecurityConfig.java
+++ src/main/java/com/smartvillage/smart_village_backend/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,7 +19,7 @@ import java.io.IOException;
 import java.util.Collections;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final FirebaseTokenService firebaseTokenService;
